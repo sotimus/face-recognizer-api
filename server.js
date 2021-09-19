7925635a990bb2805c88b8fabfc9f4c92c10bc3c
Sumitosh <sith2594@gmail.com>
2021-09-19 15:25:26 +0530

    Added the ssl rejectUnauthorized

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ const postgres = knex({
     client: 'pg',
     connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: true
+        ssl: {
+            rejectUnauthorized: false
+          }
     },
 });
 
